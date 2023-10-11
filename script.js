a90cc6750681c31556895ba3116ebcbbe2532d19
Vedant Yaduvanshi-) <87130565+vedantyaduvanshi@users.noreply.github.com>
2023-10-11 23:46:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+
+    const virendra1 = document.querySelector('#virendra1');
+    const popup1 = document.querySelector('#popup1');
+
+    virendra1.addEventListener('mouseover', () => {
+        popup1.style.visibility = 'visible';
+    });
+
+    virendra1.addEventListener('mouseout', () => {
+        popup1.style.visibility = 'hidden';
+    });
+
+    const sachin = document.querySelector('#sachin');
+    const popup2 = document.querySelector('#popup2');
+
+    sachin.addEventListener('mouseover', () => {
+        popup2.style.visibility = 'visible';
+    });
+
+    sachin.addEventListener('mouseout', () => {
+        popup2.style.visibility = 'hidden';
+    });
+
+    const gautam = document.querySelector('#gautam');
+    const popup3 = document.querySelector('#popup3');
+
+    gautam.addEventListener('mouseover', () => {
+        popup3.style.visibility = 'visible';
+    });
+
+    gautam.addEventListener('mouseout', () => {
+        popup3.style.visibility = 'hidden';
+    });
+
+    const virat = document.querySelector('#virat');
+    const popup4 = document.querySelector('#popup4');
+
+    virat.addEventListener('mouseover', () => {
+        popup4.style.visibility = 'visible';
+    });
+
+    virat.addEventListener('mouseout', () => {
+        popup4.style.visibility = 'hidden';
+    });
+
+    const msd = document.querySelector('#msd');
+    const popup5 = document.querySelector('#popup5');
+
+    msd.addEventListener('mouseover', () => {
+        popup5.style.visibility = 'visible';
+    });
+
+    msd.addEventListener('mouseout', () => {
+        popup5.style.visibility = 'hidden';
+    });
+
+
+
+
+    const yuvraj = document.querySelector('#yuvraj');
+    const popup6 = document.querySelector('#popup6');
+
+    yuvraj.addEventListener('mouseover', () => {
+        popup6.style.visibility = 'visible';
+    });
+
+    yuvraj.addEventListener('mouseout', () => {
+        popup6.style.visibility = 'hidden';
+    });
+
+
+
+    
